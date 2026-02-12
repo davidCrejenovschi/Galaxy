8f312648e263efeb0442683a36b00808b62ab9c4
davidCrejenovschi <crejenoriginal.david@gmail.com>
2026-02-12 20:14:11 +0200

    .

---
+++ transforms.py
@@ -0,0 +1,22 @@
+def transform(self, x, y):
+    #return self.transform_2d(x,y)
+    return self.transform_perspective(x, y)
+
+def transform_2d(self, x, y):
+    return int(x), int(y)
+
+def transform_perspective(self, x, y):
+    lin_y = y * self.perspective_ponit_y / self.height
+    if lin_y > self.perspective_ponit_y:
+        lin_y = self.perspective_ponit_y
+
+    diff_x = x - self.perspective_ponit_x
+    diff_y = self.perspective_ponit_y - lin_y
+
+    factor_y = diff_y / self.perspective_ponit_y
+    factor_y = pow(factor_y, 2)
+
+    tr_x = self.perspective_ponit_x + diff_x * factor_y
+    tr_y = self.perspective_ponit_y - factor_y * self.perspective_ponit_y
+
+    return int(tr_x), int(tr_y)
